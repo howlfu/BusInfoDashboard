11afb716f882ea4226f10e7f00f1cfac42dbcaa5
howlfu <howlfu@gmail.com>
2022-05-17 17:10:28 +0800

    add voice function

--- app/src/main/java/com/howlfu/businfodashboard/MainActivity.kt
+++ app/src/main/java/com/howlfu/businfodashboard/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.howlfu.businfodashboard
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import java.util.*
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         testWhenCreate()
         hideSystemUI()
+        initSpeaker()
+    }
+
+    private var tts: TextToSpeech? = null
+    private fun initSpeaker() {
+        tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val ttsLang = tts!!.setLanguage(Locale.TAIWAN)
+                if (ttsLang == TextToSpeech.LANG_MISSING_DATA || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    println( "The Language is not supported!")
+                } else {
+                    println("Language Supported.")
+                }
+                println( "Initialization success.")
+
+                Toast.makeText(applicationContext, "Initialization success.", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(applicationContext, "TTS Initialization failed!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+    private fun speak(txt: String) {
+        val speechStatus = tts!!.speak(txt, TextToSpeech.QUEUE_FLUSH, null)
+        if (speechStatus == TextToSpeech.ERROR)
+        {
+            Toast.makeText(applicationContext, "Error in converting Text to Speech", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     private fun testWhenCreate(){
@@ -27,6 +61,8 @@ class MainActivity : AppCompatActivity() {
     private fun pressTextViewHandler() {
         val text2 = findViewById<TextView>(R.id.text2)
         text2.text = getString(R.string.main_title2_add)
+
+        speak("下一站市政府")
     }
 
     private fun hideSystemUI() {
